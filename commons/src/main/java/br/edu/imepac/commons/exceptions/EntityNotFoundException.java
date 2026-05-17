c161fbe60eba29b5949d8e4bcf807b749742e5cb
Gabriel <gabrielgalaxy66@gmail.com>
2026-05-17 00:13:23 -0300

    Classe paciente criada e testada

--- commons/src/main/java/br/edu/imepac/commons/exceptions/EntityNotFoundException.java
+++ commons/src/main/java/br/edu/imepac/commons/exceptions/EntityNotFoundException.java
@@ -5,4 +5,11 @@ public class EntityNotFoundException extends RuntimeException {
     public EntityNotFoundException(String message) {
         super(message);
     }
+
+    /**
+     * Conveniência para criar mensagem baseada em entidade e id, por ex: "Paciente com id 1 não encontrado".
+     */
+    public EntityNotFoundException(String entityName, Long id) {
+        super(String.format("%s com id %d não encontrado", entityName, id));
+    }
 }
